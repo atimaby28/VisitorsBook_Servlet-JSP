89d730f0f095315ca9e334683a7a74f894521289
aquageek <aquageek28@gmail.com>
2022-08-02 14:36:01 +0900

    Update : Visitors' Book for Bean

---
+++ visitorsbook3_bean/WebContent/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Visitor</title>
+</head>
+<body>
+	<div align="center">
+		<h3>Visitors' Book with Bean!!!</h3>
+		<a href="/visitorsbook3_bean/visitorsbook/write.jsp">글쓰기</a><br> 
+		<a href="/visitorsbook3_bean/visitorsbook?act=list">글 목록</a>
+	</div>
+</body>
+</html>
---
+++ visitorsbook3_bean/src/com/visitor/visitorsbook/dto/VisitorsBookDto.java
@@ -0,0 +1,41 @@
+package com.visitor.visitorsbook.dto;
+
+public class VisitorsBookDto {
+	private int articleNo;
+	private String visitorId;
+	private String subject;
+	private String content;
+	private String regTime;
+	
+	public int getArticleNo() {
+		return articleNo;
+	}
+	public void setArticleNo(int articleNo) {
+		this.articleNo = articleNo;
+	}
+	public String getVisitorId() {
+		return visitorId;
+	}
+	public void setVisitorId(String visitorId) {
+		this.visitorId = visitorId;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegTime() {
+		return regTime;
+	}
+	public void setRegTime(String regTime) {
+		this.regTime = regTime;
+	}
+	
+}
